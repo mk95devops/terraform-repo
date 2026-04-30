74510400909cb23f8ca6731f2c661b79d4730951
mk95devops <khamidovmaruf@gmail.com>
2026-04-30 00:19:55 -0500

    fixed the sh file

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
 mkdir -p $HOME/bin
-ls -s ~/.tfenv/bin/* $HOME/bin  
+ln -s ~/.tfenv/bin/* $HOME/bin  
