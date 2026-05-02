4bf8b7a648f53a667611d4f255bca5379c271be2
mk95devops <khamidovmaruf@gmail.com>
2026-05-02 00:24:17 -0500

    added group

--- practice1/main.tf
+++ practice1/main.tf
@@ -2,7 +2,14 @@
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
 resource "aws_iam_user" "lb" {
   name = "mk"
 
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+  path = "/users/"
+  user= "mk"
 }
