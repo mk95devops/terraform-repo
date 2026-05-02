bd6f2e4593bbff50b7a393a46d52ecbed3665cd9
mk95devops <khamidovmaruf@gmail.com>
2026-05-01 23:55:40 -0500

    added provider

--- practice1/main.tf
+++ practice1/main.tf
@@ -1,7 +1,4 @@
-resource "aws_vpc" "test_vpc" {
+# Create a VPC
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    name = "test_vpc"
-
-  } 
 }
--- practice1/provider.tf
+++ practice1/provider.tf
@@ -1,3 +1,14 @@
-provider "aws" {
-  region      = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
