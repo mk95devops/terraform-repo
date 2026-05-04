576566451c0e79e43f1764491eb117e08b3470cb
mk95devops <khamidovmaruf@gmail.com>
2026-05-03 22:23:05 -0500

    added vpc tags

--- practice1/main.tf
+++ practice1/main.tf
@@ -1,6 +1,12 @@
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    name= "Practice"
+    Environment= "Dev"
+    Createdby= "MK"
+
+  }
 }
 
 resource "aws_iam_user" "lb" {
