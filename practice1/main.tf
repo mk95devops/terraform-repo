4e59cb740dad04e20723b4fbdf70750ee84ea41c
mk95devops <khamidovmaruf@gmail.com>
2026-05-03 22:12:13 -0500

    added policy

--- practice1/main.tf
+++ practice1/main.tf
@@ -11,3 +11,23 @@ resource "aws_iam_user" "lb" {
 resource "aws_iam_group" "developers" {
   name = "developers"
 }
+
+# IAM Policy 
+resource "aws_iam_policy" "test_policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
