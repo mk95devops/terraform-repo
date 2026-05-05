7afb0f336589a2d131b05e5a57a82703171127a0
mk95devops <khamidovmaruf@gmail.com>
2026-05-05 00:41:13 -0500

    added instance

--- practice1/main.tf
+++ practice1/main.tf
@@ -72,3 +72,29 @@ resource "aws_key_pair" "deployer" {
   public_key = file("~/.ssh/id_rsa.pub")
   tags = local.common_tags
 }
+
+# Creating EC2 Instance 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
