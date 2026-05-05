01976aa96585c44148f917b5606d91e9b70b063e
mk95devops <khamidovmaruf@gmail.com>
2026-05-05 01:05:54 -0500

    removed ec2

--- practice1/main.tf
+++ practice1/main.tf
@@ -73,28 +73,3 @@ resource "aws_key_pair" "deployer" {
   tags = local.common_tags
 }
 
-# Creating EC2 Instance 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
