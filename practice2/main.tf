4a3b9c585b713517e4a3b9e24288c78b920a6eff
mk95devops <khamidovmaruf@gmail.com>
2026-05-05 14:29:51 -0500

    added sec group

--- practice2/main.tf
+++ practice2/main.tf
@@ -19,6 +19,7 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   key_name = aws_key_pair.practice2.key_name
   user_data = file("installweb.sh")
+  vpc_security_group_ids = aws_security_group.web_sg.id
 
   tags = {
     Name = "HelloWorld"
@@ -28,4 +29,31 @@ resource "aws_instance" "example" {
 resource "aws_key_pair" "practice2" {
   key_name   = "practice2"
   public_key = file("~/.ssh/id_rsa.pub")
+}
+# 1. Define the Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "allow_web_traffic"
+  description = "Allow port 80 and 443"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
