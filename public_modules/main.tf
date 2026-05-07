341db164abeb6ed39942d1abe369081974e5fe4a
mk95devops <khamidovmaruf@gmail.com>
2026-05-06 23:17:27 -0500

    added subnets

--- public_modules/main.tf
+++ public_modules/main.tf
@@ -69,8 +69,9 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "my-alb"
-  vpc_id  = "vpc-abcde012"
-  subnets = ["subnet-abcde012", "subnet-bcde012a"]
+  vpc_id  = "my-vpc"
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   # Security Group
   security_group_ingress_rules = {
