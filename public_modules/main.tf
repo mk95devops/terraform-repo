bf262499bba45d3964910cef9a37c6fca2dfaf3c
mk95devops <khamidovmaruf@gmail.com>
2026-05-06 21:51:54 -0500

    added lb

--- public_modules/main.tf
+++ public_modules/main.tf
@@ -62,3 +62,79 @@ module "asg" {
   enable_monitoring = false
 
 }
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "my-alb"
+  vpc_id  = "vpc-abcde012"
+  availability_zones = ["us-east-2a","us-east-2b","us-east-2c"]
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+resource "aws_autoscaling_attachment" "asg_alb" {
+  autoscaling_group_name = aws_autoscaling_group.example-asg.name
+  lb_target_group_arn    = module.alb.target_groups["ex-instance"].arn
+}
