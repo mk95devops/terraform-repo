a46eccebbcf1f0c6efa4a493b3c79f0452964dfb
mk95devops <khamidovmaruf@gmail.com>
2026-05-06 23:21:38 -0500

    added vpc info

--- public_modules/main.tf
+++ public_modules/main.tf
@@ -69,9 +69,9 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "my-alb"
-  vpc_id  = "my-vpc"
-    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    vpc_id = module.vpc.vpc_id
+
+  subnets = module.vpc.public_subnets
 
   # Security Group
   security_group_ingress_rules = {
