45cf7fdfed997140e61125bdcd66cfe7c1a2daf3
mk95devops <khamidovmaruf@gmail.com>
2026-05-13 20:53:20 -0500

    added ec2

--- practice4/main.tf
+++ practice4/main.tf
@@ -6,3 +6,28 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
