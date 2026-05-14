1cfa4cf545f29f41700fd7bb04a6ea8ac348391a
mk95devops <khamidovmaruf@gmail.com>
2026-05-13 20:50:15 -0500

    added var

--- practice4/provider.tf
+++ practice4/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "4.1"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 }
 
