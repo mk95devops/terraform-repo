9025e2a57d5d752da7b8d1f42c657bdfd31b3c23
mk95devops <khamidovmaruf@gmail.com>
2026-05-15 14:37:31 -0500

    fixed the code

--- practice4/main.tf
+++ practice4/main.tf
@@ -28,14 +28,14 @@ resource "aws_subnet" "subnet1" {
 }
 
 resource "aws_subnet" "subnet2" {
-  availability_zone = data.aws_availability_zones.available.names[2]
+  availability_zone = data.aws_availability_zones.available.names[1]
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 
 }
 
 resource "aws_subnet" "subnet3" {
-  availability_zone = data.aws_availability_zones.available.names[3]
+  availability_zone = data.aws_availability_zones.available.names[2]
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
 
