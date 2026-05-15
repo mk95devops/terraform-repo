f15817a694db8b786430c70801a3a781dee30d55
mk95devops <khamidovmaruf@gmail.com>
2026-05-15 14:31:46 -0500

    added availability zones

--- practice4/main.tf
+++ practice4/main.tf
@@ -10,3 +10,33 @@ resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = file("~/.ssh/id_rsa.pub")
 }
+
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+
+
+
+resource "aws_subnet" "subnet1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+}
+
+resource "aws_subnet" "subnet2" {
+  availability_zone = data.aws_availability_zones.available.names[2]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+}
+
+resource "aws_subnet" "subnet3" {
+  availability_zone = data.aws_availability_zones.available.names[3]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+
+}
